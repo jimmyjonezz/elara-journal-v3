de0cfcae98dda0284e7038dd9bb76ac1fecc1c85
Jimmy <antonka@vk.com>
2026-03-24 17:47:42 +0300

    Update embedding.service.ts

--- services/embedding.service.ts
+++ services/embedding.service.ts
@@ -1,8 +1,11 @@
 // ---------- /services/embedding.service.ts ----------
 import { EmbeddingService } from "../interfaces/embedding"
+import { OpenAIClient } from "../infra/llm/openai.client"
 
-export class MockEmbeddingService implements EmbeddingService {
-  async embed(): Promise<number[]> {
-    return [0, 0, 0]
+export class OpenAIEmbeddingService implements EmbeddingService {
+  constructor(private client: OpenAIClient) {}
+
+  async embed(text: string): Promise<number[]> {
+    return this.client.embed(text)
   }
 }
