0872698e257bea2fd5760ac304008794810afff3
Jimmy <antonka@vk.com>
2026-03-24 22:11:41 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,27 @@
-// ---------- /main.ts ----------
 import { JournalEngine } from "./core/journalEngine"
 
 import { JsonMemoryService } from "./services/memory.service"
-//import { SQLiteEntryRepository } from "./infra/db/entry.repository"
-
 import { AIGenerator } from "./services/generator.service"
 import { AIReflector } from "./services/reflector.service"
-
 import { SimpleEvaluator } from "./services/evaluator.service"
 import { ConsolePublisher } from "./services/publisher.service"
 
-//import { OpenAIClient } from "./infra/llm/openai.client"
+import { OllamaClient } from "./infra/llm/ollama.client"
 import { OllamaEmbeddingService } from "./services/embedding.service"
 import { FilePromptManager } from "./services/prompt.service"
-import { OllamaClient } from "./infra/llm/ollama.client"
 
 async function main() {
-  //const llm = new OpenAIClient()
   const llm = new OllamaClient()
   const prompts = new FilePromptManager()
 
-  //const repo = new SQLiteEntryRepository()
-  //const memory = new JsonMemoryService()
+  const embedding = new OllamaEmbeddingService(llm)
+  const memory = new JsonMemoryService(embedding)
 
-  const generator = new AIGenerator(llm, prompts)
+  const generator = new AIGenerator(llm, prompts, memory)
   const reflector = new AIReflector(llm, prompts)
 
   const evaluator = new SimpleEvaluator()
   const publisher = new ConsolePublisher()
-  //const embedding = new OpenAIEmbeddingService(llm)
-  const embedding = new OllamaEmbeddingService(llm)
-  const memory = new JsonMemoryService(embedding)
 
   const engine = new JournalEngine(
     memory,
