1e945180f232431b64a844d537de6eec0ae7fc6a
Jimmy <antonka@vk.com>
2026-03-25 10:05:32 +0300

    Update embedding.service.ts

--- services/embedding.service.ts
+++ services/embedding.service.ts
@@ -1,8 +1,8 @@
 import { EmbeddingService } from "../interfaces/embedding"
-import { OllamaClient } from "../infra/llm/ollama.client"
+import { VoyageClient } from "../infra/llm/voyage.client"
 
-export class OllamaEmbeddingService implements EmbeddingService {
-  constructor(private client: OllamaClient) {}
+export class VoyageEmbeddingService implements EmbeddingService {
+  constructor(private client: VoyageClient) {}
 
   async embed(text: string): Promise<number[]> {
     return this.client.embed(text)
